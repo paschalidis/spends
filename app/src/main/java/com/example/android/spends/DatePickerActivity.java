be1d58ae447040c4c0993f9ff48e612fe7ecf5fb
paschalidis <msf7415012@teilar.gr>
2017-01-30 07:23:31 +0200

    Update picker

--- app/src/main/java/com/example/android/spends/DatePickerActivity.java
+++ app/src/main/java/com/example/android/spends/DatePickerActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.spends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
 
 public class DatePickerActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class DatePickerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_picker);
+        setTitle("Date Picker");
+    }
+
+    public void orderedList(View view){
+
+        CalendarView calendarFrom = (CalendarView) findViewById(R.id.date_from);
+        long from = calendarFrom.getDate() / 1000L;
+
+        CalendarView calendarTo = (CalendarView) findViewById(R.id.date_to);
+
+        long to = calendarTo.getDate() / 1000L;
+
+        Intent orderedListActivity = new Intent(this, OrderedListActivity.class);
+        startActivity(orderedListActivity);
     }
 }
---
+++ app/src/main/java/com/example/android/spends/OrderedListActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.spends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderedListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordered_list);
+        setTitle("Ordered List");
+    }
+}

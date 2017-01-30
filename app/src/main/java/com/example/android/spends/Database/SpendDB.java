0fa086952e4d43db52065ccab701a652c41742bc
paschalidis <msf7415012@teilar.gr>
2017-01-30 20:18:39 +0200

    Update SpendDB
    Modify orderd list with custom selct and orderd list

--- app/src/main/java/com/example/android/spends/Database/SpendDB.java
+++ app/src/main/java/com/example/android/spends/Database/SpendDB.java
@@ -104,6 +104,20 @@ public class SpendDB {
         SpendDbHelper mDbHelper = new SpendDbHelper(this.context);
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
+        /*
+        SELECT *,
+        CAST(amount AS DECIMAL (10, 2)) AS _amount 
+        FROM spends
+        WHERE spends.date >= z
+        AND spends.date <= c
+        GROUP BY spends.category.id
+        ORDER BY _amount DESC
+        
+        String sqlQuery = "SELECT * FROM " + SpendEntry.TABLE_NAME + " WHERE " + SpendEntry._ID
+                        + " = " + id;
+        Cursor cursor = db.rawQuery(sqlQuery, null);
+        */
+        
         // Filter results WHERE "title" = 'My Title'
         String selection = SpendEntry.COLUMN_DATE + " BETWEEN ?" + " AND " + " ?";
 
